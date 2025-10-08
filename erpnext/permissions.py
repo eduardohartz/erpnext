00d3e48d306662b73bf93f9d94f6966e82fb22ef
Eduardo Hartz <80062347+eduardohartz@users.noreply.github.com>
2025-10-07 18:35:08 -0700

    Fix formatting and indentation in permissions.py

--- erpnext/permissions.py
+++ erpnext/permissions.py
@@ -1,14 +1,16 @@
 import frappe
 
+
 def crm_lead_has_permission(doc, ptype, user):
     # Always allow system-level users / roles
-	user_doc = frappe.get_doc('User', frappe.user)
-	has_special_role = [r for r in user_doc.roles if r.role == 'System Manager']
+    user_doc = frappe.get_doc("User", frappe.user)
+    has_special_role = [r for r in user_doc.roles if r.role == "System Manager"]
+
     if user == "Administrator" or has_special_role:
         return True
 
     # Allow creation (even if ptype is None)
-    if (ptype == "create" or ptype is None):
+    if ptype == "create" or ptype is None:
         return True
 
     # Allow if user is the owner
